f0239fb350b718706c9b1b13012bacf8c4dfdf66
YAMASAKI Masahide <masahide.y@gmail.com>
2017-01-29 22:59:58 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dghubble/oauth1"
+)
+
+const (
+	timelineEndpoint  = `https://api.twitter.com/1.1/statuses/user_timeline.json`
+	streamingEndpoint = `https://stream.twitter.com/1.1/statuses/filter.json`
+	verifyEndpoint    = `https://api.twitter.com/1.1/account/verify_credentials.json`
+)
+
+type TwitterConfig struct {
+	Config *oauth1.Config
+	Token  *oauth1.Token
+}
+
+func main() {
+
+	conf := TwitterConfig{}
+
+	jsonString, err := ioutil.ReadFile("apikeys.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(jsonString, &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	httpClient := conf.Config.Client(ctx, conf.Token)
+
+	queryString := `?track=pig`
+	req, err := http.NewRequest("POST", streamingEndpoint+queryString, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+
+}
